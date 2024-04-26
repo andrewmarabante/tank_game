f1ef2771b85f190d3fd6c9b90434be7079627394
andrewmarabante <andrewmarabante@gmail.com>
2024-04-26 01:58:15 -0700

    connected socket.io

---
+++ backend/app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var socket = require('socket.io');
+var http = require('http');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var cors = require('cors')
+
+var app = express();
+
+var server = http.createServer(app)
+
+server.listen(4000, ()=>{
+  console.log('server listening on port 4000')
+})
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+
+  socket.on('click', function(data){
+    io.sockets.emit('click', data)
+  })
+})
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import io from 'socket.io-client'
+
+function App() {
+  const [socket,setSocket] = useState(null)
+  const [message, setMessage] = useState('old')
+
+  useEffect(()=>{
+    var socketInstance = io('http://localhost:4000', { transports : ['websocket'] });
+    setSocket(socketInstance);
+
+    socketInstance.on('connect', () => {
+      console.log('Connected to server');
+
+      socketInstance.on('click', function(data){
+        console.log(data.message)
+        const newMessage = data.message;
+        setMessage(newMessage)
+      })
+    });
+  
+    return () => {
+      socketInstance.off('click');
+    };
+  },[]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('click', function (data) {
+        console.log(data.message);
+        const newMessage = data.message;
+        setMessage(newMessage);
+      });
+    }
+  }, [socket]);
+
+
+function handleClick(){
+  console.log('handler')
+  socket.emit('click', {
+    message: 'new'
+  })
+}
+
+  return (
+    <div>
+      <div>{message}</div>
+      <button onClick={handleClick}>Do Something</button>
+    </div>
+  )
+}
+
+export default App
